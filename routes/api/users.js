a32329916680c2caa69ad2c85cf42fe2fde64633
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-06-21 12:50:50 +0600

    Create User model and set user validation also

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("User Router");
-});
+router.get(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more character"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+    res.send("User Router");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 // connect Database
 connectDB();
 
+// middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
