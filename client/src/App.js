114cabf4b9434e5edbc684d722362a2700a4af43
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-06 17:50:57 +0600

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 
@@ -6,7 +9,16 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <Landing></Landing>
+
+      <Routes>
+        <Route path="/" element={<Landing></Landing>}></Route>
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+        </Routes>
+      </section>
     </>
   );
 }
